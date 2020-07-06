005012f6560056979bf3ce4d3dad16202ab0193b
yChystov <misteriysmo@gmail.com>
2020-07-06 11:42:54 +0300

    split tests

---
+++ src/test/java/com/rozetka/tests/RozetkaLoginTest.java
@@ -0,0 +1,34 @@
+package com.rozetka.tests;
+
+import com.rozetka.core.BaseTest;
+import com.rozetka.data.RozetkaData;
+import com.rozetka.pages.RozetkaLoginPage;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertNotEquals;
+
+@Listeners(com.rozetka.utils.ListenerTest.class)
+public class RozetkaLoginTest extends BaseTest {
+
+    RozetkaLoginPage rbMainPage;
+
+    @Test(dataProviderClass = RozetkaData.class, dataProvider = "login")
+    public void loginTest(String email, String pass, String result) {
+        rbMainPage = PageFactory.initElements(getDriver(), RozetkaLoginPage.class);
+        //login
+        rbMainPage.loadPage();
+        rbMainPage.login(email, pass);
+        assertNotEquals(result, rbMainPage.getUserName());
+    }
+
+    @Test
+    public void loginTestWithoutDataProvider() {
+        rbMainPage = PageFactory.initElements(getDriver(), RozetkaLoginPage.class);
+        //login
+        rbMainPage.loadPage();
+        rbMainPage.login("email@gmail.com", "trololo");
+        assertNotEquals("result", rbMainPage.getUserName());
+    }
+}
--- src/test/java/com/rozetka/tests/RozetkaSearchTest.java
+++ src/test/java/com/rozetka/tests/RozetkaSearchTest.java
@@ -3,30 +3,21 @@ package com.rozetka.tests;
 import com.rozetka.core.BaseTest;
 import com.rozetka.data.RozetkaData;
 import com.rozetka.pages.RozetkaCheckoutPage;
-import com.rozetka.pages.RozetkaLoginPage;
 import com.rozetka.pages.RozetkaSearchPage;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertNotEquals;
-
 @Listeners(com.rozetka.utils.ListenerTest.class)
-public class RozetkaBaseTest extends BaseTest {
+public class RozetkaSearchTest extends BaseTest {
 
-    @Test(dataProviderClass = RozetkaData.class, dataProvider = "login")
-    public void loginTest(String email, String pass, String result) {
-        RozetkaLoginPage rbMainPage = PageFactory.initElements(getDriver(), RozetkaLoginPage.class);
-        //login
-        rbMainPage.loadPage();
-        rbMainPage.login(email, pass);
-        assertNotEquals(result, rbMainPage.getUserName());
-    }
+    RozetkaSearchPage rSearchPage;
+    RozetkaCheckoutPage rCheckoutPage;
 
     @Test(dataProviderClass = RozetkaData.class, dataProvider = "search")
     public void searchTest(String item, String status) {
-        RozetkaSearchPage rSearchPage = PageFactory.initElements(getDriver(), RozetkaSearchPage.class);
-        RozetkaCheckoutPage rCheckoutPage = PageFactory.initElements(getDriver(), RozetkaCheckoutPage.class);
+        rSearchPage = PageFactory.initElements(getDriver(), RozetkaSearchPage.class);
+        rCheckoutPage = PageFactory.initElements(getDriver(), RozetkaCheckoutPage.class);
         //search
         rSearchPage.loadPage();
         rSearchPage.setSearchText(item);
